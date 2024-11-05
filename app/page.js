47b0211df1813d97f1a3169d6f5caa49a703a4c2
aerdni99 <adi19@uakron.edu>
2024-11-04 22:24:55 -0500

    Fix fast refresh file and component naming issue

--- app/page.js
+++ app/page.js
@@ -1,24 +1,20 @@
 // Main page
 
-import LikeButton from './like-button';
-import Header from './header'
+import LikeButton from './LikeButton';
+import Head from './Head'
 
 export default function HomePage() {
     const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
 
     return (
-      <div>
-        <Header title='Develop. Preview. Ship.'/>
-        <ul>
-            {names.map((name) => (
-                <li key={name}>{name}</li>
-            ))}
-        </ul>
-        <LikeButton />
-        <LikeButton />
-        <LikeButton />
-        <LikeButton />
-        <LikeButton />
-      </div>
+        <div>
+          <Head title='Develop. Preview. Ship.'/>
+          <ul>
+              {names.map((name) => (
+                  <li key={name}>{name}</li>
+              ))}
+          </ul>
+          <LikeButton />
+        </div>
     );
 }
