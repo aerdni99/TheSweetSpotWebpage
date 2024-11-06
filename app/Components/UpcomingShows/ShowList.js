d0e3594c2ae99866d231779ff8785239fc63040c
aerdni99 <adi19@uakron.edu>
2024-11-05 23:06:17 -0500

    Add files for listing bands upcoming shows

---
+++ app/Components/UpcomingShows/ShowList.js
@@ -0,0 +1,10 @@
+/*
+    ShowList.js
+
+    Component to list upcoming shows for the band
+
+    Gonna use the BandsInTown Events Widget for this
+    Documentation:
+        https://www.artists.bandsintown.com/support/blog/install-events-widget
+        https://help.artists.bandsintown.com/en/articles/9186477-api-documentation
+*/
