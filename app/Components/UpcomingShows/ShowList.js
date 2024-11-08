c7e27b3d091013f2b51b7eb8c7812aba6988de4d
aerdni99 <adi19@uakron.edu>
2024-11-08 15:13:36 -0500

    extract title1 and title2 to global style sheet

--- app/Components/UpcomingShows/ShowList.js
+++ app/Components/UpcomingShows/ShowList.js
@@ -8,3 +8,30 @@
         https://www.artists.bandsintown.com/support/blog/install-events-widget
         https://help.artists.bandsintown.com/en/articles/9186477-api-documentation
 */
+
+"use client"
+
+export default function ShowList() {
+    return (
+        <div>
+            <p>test</p>
+            <script charSet="utf-8" src="https://widget.bandsintown.com/main.min.js"></script>
+            <a 
+                class="bit-widget-initializer" 
+                data-artist-name="The Sweet Spot" 
+                data-display-local-dates="true" 
+                data-display-past-dates="false" 
+                data-auto-style="false" 
+                data-text-color="#FFFFFF" 
+                data-link-color="#00b4b3" 
+                data-background-color="rgba(0,0,0,0)" 
+                data-display-limit="10" 
+                data-display-start-time="true" 
+                data-link-text-color="#FFFFFF" 
+                data-display-lineup="true" 
+                data-display-play-my-city="true" 
+                data-separator-color="rgba(124,124,124,0.25)"
+            ></a>
+        </div>
+    );
+}
--- app/page.js
+++ app/page.js
@@ -7,12 +7,14 @@
 import './globals.css';
 import PhotoAlbum from './Components/PhotoAlbum/PhotoAlbum.js'
 import VideoList from './Components/VideoList/VideoList.js'
+import ShowList from './Components/UpcomingShows/ShowList.js'
 
 export default function HomePage() {
 
     return (
     <div>
         <PhotoAlbum />
+        <ShowList />
         <VideoList/>
     </div>
     );
