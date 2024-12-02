f1f3d278f4674da08df5647b099568d9d3f47324
aerdni99 <adi19@uakron.edu>
2024-12-02 15:10:58 -0500

    Add new photos to webpage

--- app/Components/PhotoAlbum/PhotoAlbum.js
+++ app/Components/PhotoAlbum/PhotoAlbum.js
@@ -10,46 +10,64 @@ import Image from 'next/image';
 import './PhotoAlbum.css';
 
 export default function PhotoAlbum() {
-    const img_paths = ['/band/peek-over.jpg',
+    const img_paths = [
         '/band/chalk-clap.jpg',
-        '/band/stage-sit.jpg',
-        '/band/maddie-corral.jpg',
-        '/band/over-under-2.jpg'];
+        '/band/bro-sis.jpg',
+        '/band/ball-above.jpg',
+        '/band/cocktails.jpg',
+        '/band/chalk-logo.jpg',
+        '/band/bro-sis-2.jpg',
+        '/band/chalk-clap-2.jpg'
+    ];
 
     return (
         <div className='wrapper'>
             <Image
                 src={img_paths[0]}
                 alt='Band Pictures'
-                width={375}
+                width={250}
                 height={250}
                 className='photo'
             />
             <Image
                 src={img_paths[1]}
                 alt='Band Pictures'
-                width={375}
+                width={250}
                 height={250}
                 className='photo'
             />
             <Image
                 src={img_paths[2]}
                 alt='Band Pictures'
-                width={375}
+                width={250}
                 height={250}
                 className='photo'
             />
             <Image
                 src={img_paths[3]}
                 alt='Band Pictures'
-                width={375}
+                width={250}
                 height={250}
                 className='photo'
             />
             <Image
                 src={img_paths[4]}
                 alt='Band Pictures'
-                width={375}
+                width={250}
+                height={250}
+                className='photo'
+            />
+            <Image
+                src={img_paths[5]}
+                alt='Band Pictures'
+                width={250}
+                height={250}
+                className='photo'
+            />
+            <Image
+                src={img_paths[6]}
+                alt='Band Pictures'
+                width={250}
                 height={250}
                 className='photo'
             />
