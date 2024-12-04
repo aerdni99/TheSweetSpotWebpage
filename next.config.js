d1e4ef96e7c6433c5a5de9560ff67a57ce85f7eb
aerdni99 <adi19@uakron.edu>
2024-12-04 18:29:36 -0500

    Attempt to deploy to Vercel

--- next.config.js
+++ next.config.js
@@ -5,15 +5,20 @@
 */
 
 module.exports = {
-    reactStrictMode: true, // This enables React's Strict Mode
+    reactStrictMode: true,
     webpack: (config, { dev }) => {
       if (dev) {
         config.watchOptions = {
-          poll: 1000, // Check for changes every second
-          aggregateTimeout: 300, // Delay rebuild after the first change
+          poll: 1000,
+          aggregateTimeout: 300,
         };
       }
       return config;
     },
   };
   
+  /*
+    reactStrictMode: true, // This enables React's Strict Mode
+    poll: 1000, // Check for changes every second
+    aggregateTimeout: 300, // Delay rebuild after the first change
+*/
