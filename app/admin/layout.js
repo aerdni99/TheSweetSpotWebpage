d983c512727fd3a794bf32035a6758164b8b19c6
aerdni99 <adi19@uakron.edu>
2025-01-30 17:02:16 -0500

    Add app/admin/layout.js

---
+++ app/admin/layout.js
@@ -0,0 +1,14 @@
+/*
+    app/admin/layout.js
+
+    The base layout of the admin page will be different from the layout of the main band page.
+    This also allows me to make session providers wrap only the admin page instead of the entire application.
+*/
+
+"use client";
+
+import { SessionProvider } from "next-auth/react";
+
+export default function AdminLayout({ children }) {
+  return <SessionProvider>{children}</SessionProvider>;
+}
