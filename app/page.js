64a8dae80e0a6a7a198fcc773ed2c4ae0336b0a6
aerdni99 <adi19@uakron.edu>
2025-02-04 03:17:27 -0500

    Update dependencies

--- app/page.js
+++ app/page.js
@@ -6,19 +6,24 @@
 */
 
 import './globals.css';
-import PhotoAlbum from './Components/PhotoAlbum/PhotoAlbum.js'
-import VideoList from './Components/VideoList/VideoList.js'
-import ShowList from './Components/UpcomingShows/ShowList.js'
+import PhotoAlbum from './Components/PhotoAlbum/PhotoAlbum.js';
+import VideoList from './Components/VideoList/VideoList.js';
+import ShowList from './Components/UpcomingShows/ShowList.js';
 import BookingInquiry from './Components/BookingInquiry/BookingInquiry.js';
 import AboutSection from './Components/AboutSection/AboutSection.js';
+import path from 'path';
+import fs from 'fs';
 
 
 export default function HomePage() {
+    
+    const filePath = path.join(process.cwd(), 'public', 'bio.txt');
+    const bio = fs.readFileSync(filePath, 'utf-8');
 
     return (
     <div>
         <PhotoAlbum />
-        <AboutSection />
+        <AboutSection bio={bio} />
         <ShowList />
         <BookingInquiry />
         <VideoList/>
