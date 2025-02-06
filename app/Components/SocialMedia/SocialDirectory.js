090511e134f3fa14d39f1e0631df7e0573a474cd
aerdni99 <adi19@uakron.edu>
2025-02-06 17:54:49 -0500

    Change social icons to .svg's

--- app/Components/SocialMedia/SocialDirectory.js
+++ app/Components/SocialMedia/SocialDirectory.js
@@ -12,10 +12,10 @@ import './Socials.css'
 export default function SocialDirectory() {
     return (
         <div className="navBar">
-            <SocialButton path='/socials/facebook.png' link='https://www.facebook.com/indreband/' alt='Facebook'/>
-            <SocialButton path='/socials/instagram.png' link='https://www.instagram.com/thesweetspot_band/' alt='Instagram'/>
-            <SocialButton path='/socials/spotify.png' link='https://open.spotify.com/artist/1zGfVEcjrkEJUvCqbzKgV2' alt='Spotify'/>
-            <SocialButton path='/socials/youtube.png' link='https://www.youtube.com/@thesweetspotband' alt='Youtube'/>
+            <SocialButton path='/socials/facebook.svg' link='https://www.facebook.com/indreband/' alt='Facebook' />
+            <SocialButton path='/socials/instagram.svg' link='https://www.instagram.com/thesweetspot_band/' alt='Instagram' />
+            <SocialButton path='/socials/spotify.svg' link='https://open.spotify.com/artist/1zGfVEcjrkEJUvCqbzKgV2' alt='Spotify' />
+            <SocialButton path='/socials/youtube.svg' link='https://www.youtube.com/@thesweetspotband' alt='Youtube' />
             {/* <SocialButton path='/socials/apple-music.png' link='https://music.apple.com/gb/artist/indr%C3%A9/1439515994'  alt='Apple Music'/> */}
         </div>
     );
