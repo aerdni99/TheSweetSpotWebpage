9551e7ddf8e7cb9ae0d721d44754e88eb0ab77a1
aerdni99 <adi19@uakron.edu>
2025-04-03 01:43:40 -0400

    Progress on logging emails

--- Components/Mailing List/MailingList.js
+++ Components/Mailing List/MailingList.js
@@ -4,15 +4,30 @@
     Component file for the mailing list component
 */
 
+"use client";
+
+import { useState } from "react";
+
 export default function MailingList() {
+    const [email, setEmail] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Email submitted: ", email);
+    }
     return (
         <div className="flex flex-col items-center drop-shadow-[0_0_10px_#f7e] neonText" style={{ backgroundColor: '#332929' }}>
-            <form className="flex flex-col items-center gap-3 p-4 max-w-sm">
+            <form 
+                onSubmit={ handleSubmit } 
+                className="flex flex-col items-center gap-3 p-4 max-w-sm"
+            >
                 <h2 className="text-2xl">Join Our Mailing List</h2>
                 <div className="flex flex-row">
                     <input
                         type="email"
                         placeholder="Enter your email"
+                        value={ email }
+                        onChange={(e) => setEmail(e.target.value)}
                         className="flex-grow p-2 border rounded-l-md focus:outline-none focus:ring-2 focus:ring-pink-300"
                     />
                     <button
