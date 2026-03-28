b5e5e4dfc75c5c5e155063af62c9ed1665bf617b
aerdni99 <aerdni99@gmail.com>
2026-03-27 21:41:27 -0400

    Add files for show selector and client side db access

---
+++ Components/PhotoAlbum/ShowSelector.js
@@ -0,0 +1,41 @@
+/*
+    ShowSelector.js
+
+    This component is for the dropdown selector for cycling between different photo albums
+*/
+
+"use client";
+import { useState } from "react";
+import { createClient } from "../../utils/supabase/client";
+
+// This function will need passed a list of shows we've played. It will be a pair of values concatenated
+// Venue + Date. 
+export default function ShowSelector() {
+    const [open, setOpen] = useState(false);
+
+    // This needs to be initialized to a random show we have played before
+    const [selected, setSelected] = useState("Britewinter - Cleveland, OH");
+
+    const supabase = createClient();
+    const [data, setData] = useState([]);
+
+    const handleUpdate = async () => {
+        if (selected === "Britewinter - Cleveland, OH") {
+            setSelected("The Windsor - Windsor, WI");
+        } else {
+            setSelected("Britewinter - Cleveland, OH");
+        }
+        const { incomingData } = await supabase.from('Photos').select(selected);
+        setData(incomingData);
+    }
+
+    return (
+        <div className="relative w-48">
+        <h1 className='neonText text-4xl text-center p-4'>Venue Name</h1>
+            <button
+                onClick={handleUpdate}>
+
+                </button>
+        </div>
+    )
+}
